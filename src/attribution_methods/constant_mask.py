c5063d50bf1406737475c2130802434b918b0644
Yang Zhang <ya.zhang@tum.de>
2021-08-31 22:43:31 +0100

    bugfix

--- src/attribution_methods/constant_mask.py
+++ src/attribution_methods/constant_mask.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 
-def compute_constant_mask(model, preprocessed_image, label):
+def compute_constant_mask(model, preprocessed_image, label, baseline=None):
     # TODO change later
     # grad = torch.rand_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
     return grad
--- src/attribution_methods/random_mask.py
+++ src/attribution_methods/random_mask.py
@@ -2,6 +2,6 @@ import numpy as np
 import torch
 
 
-def compute_random_mask(model, preprocessed_image, label):
+def compute_random_mask(model, preprocessed_image, label, baseline=None):
     grad = torch.rand_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
     return grad
