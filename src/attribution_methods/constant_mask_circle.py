e10514fbacd8bdbb68b1d90672496e2bd694519f
Yang Zhang <ya.zhang@tum.de>
2021-09-01 14:53:51 +0100

    bugfix

--- src/attribution_methods/constant_mask_circle.py
+++ src/attribution_methods/constant_mask_circle.py
@@ -5,7 +5,7 @@ from skimage import filters
 
 def compute_constant_mask_circle(model, preprocessed_image, label, baseline=None):
     assert preprocessed_image.shape[-2] == preprocessed_image.shape[-1], "the image has different length and height"
-    grad = torch.zeros_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
+    grad = torch.zeros_like(preprocessed_image).detach().cpu().clone().numpy()
     image_length = list(grad.shape)[-1]
     rr, cc = circle(image_length//2, image_length//2, image_length//6)
     grad[:, :, rr, cc] = 1
