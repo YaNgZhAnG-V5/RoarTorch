90f35cf4b2b4f9f26eac15ecf0c82ee474f3e4ed
Yang Zhang <ya.zhang@tum.de>
2021-09-01 11:03:52 +0100

    add constant mask (all ones)

--- src/attribution_methods/constant_mask.py
+++ src/attribution_methods/constant_mask.py
@@ -3,6 +3,5 @@ import torch
 
 
 def compute_constant_mask(model, preprocessed_image, label, baseline=None):
-    # TODO change later
-    # grad = torch.rand_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
+    grad = torch.ones_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
     return grad
