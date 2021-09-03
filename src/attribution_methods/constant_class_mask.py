8802fe86c1f0bab9b21d5f521a79af6969bef926
Yang Zhang <ya.zhang@tum.de>
2021-09-03 17:34:06 +0100

    complete constatnt class mask

--- src/attribution_methods/constant_class_mask.py
+++ src/attribution_methods/constant_class_mask.py
@@ -9,17 +9,16 @@ def compute_constant_class_mask(model, preprocessed_image, label, baseline=None)
 
     # get num of classes
     num_of_classes = model(preprocessed_image).shape[1]
-    classes_per_row = torch.floor(torch.sqrt(torch.tensor([num_of_classes]))).item()
-    print(classes_per_row)
+    classes_per_row = torch.ceil(torch.sqrt(torch.tensor([num_of_classes], dtype=torch.float64))).item()
 
     # get label
     label_idx = label.item()
 
     # get image length
+    grad = torch.zeros_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
     image_length = list(grad.shape)[-1]
 
     # construct circle based on label
-    grad = torch.zeros_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
     center_x = image_length * ((label_idx%classes_per_row)+1)/(classes_per_row+1)
     center_y = image_length * ((label_idx//classes_per_row)+1)/(classes_per_row+1)
     rr, cc = circle(center_x, center_y, image_length // (classes_per_row*2))
