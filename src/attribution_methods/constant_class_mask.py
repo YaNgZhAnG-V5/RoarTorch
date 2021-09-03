01e7f858acf9d75a909c64002cca255f26077051
Yang Zhang <ya.zhang@tum.de>
2021-09-03 16:33:03 +0100

    add constant class mask (incomplete)

---
+++ src/attribution_methods/constant_class_mask.py
@@ -0,0 +1,18 @@
+import numpy as np
+import torch
+from skimage.draw import circle
+from skimage import filters
+
+def compute_constant_mask_circle(model, preprocessed_image, label, baseline=None):
+    assert preprocessed_image.shape[-2] == preprocessed_image.shape[-1], "the image has different length and height"
+
+    # get num of classes
+    score = model(preprocessed_image)
+    print(score.shape)
+    print(label)
+    grad = torch.zeros_like(preprocessed_image).detach().cpu().clone().numpy().squeeze()
+    image_length = list(grad.shape)[-1]
+    rr, cc = circle(image_length//2, image_length//2, image_length//6)
+    grad[:, rr, cc] = 1
+    grad = filters.gaussian(grad)
+    return grad
